d3bde645bc8dd23f76f5c7b2cb3f4db0bbcd98a5
StevenINSA <>
2021-03-31 09:57:00 +0200

    ajout du premier fichier

--- mastermind.py
+++ mastermind.py
@@ -1,1 +1,4 @@
 from random import randint
+
+#Les 8 couleurs disponibles sont : (rouge, jaune, bleu, orange, vert, blanc, violet, rose)
+couleur = ["Rouge", "Jaune", "Bleu", "Orange", "Vert", "Blanc", "Violet", "Rose"]
